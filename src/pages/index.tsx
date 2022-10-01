600d3d434f7d9a24468085106369853efd092ee8
Joao Maranhao <joaofelipemaranhao@gmail.com>
2022-10-01 05:11:24 -0300

    feat: create buttons commands to interact with api

---
+++ src/pages/index.tsx
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+
+import styles from '../styles/Home.module.css'
+import animation from '../../public/empty-box.json'
+
+export default function Home() {
+  const [events, setEvents] = useState(null)
+
+  useEffect(() => {
+    axios.get('http://localhost:3333/api/events')
+      .then(res => {
+        setEvents(res.data)
+      })
+  }, [])
+  
+  return (
+    <>
+      <Head>
+        <title>Event Manager</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <h1 className={styles.title}>Event Manager</h1>
+          <div className={styles.commands}>
+            <button className={styles.command}>
+              <h2>Events</h2>
+              <p>View all events</p>
+            </button>
+            <button className={styles.command}>
+              <h2>Add Event</h2>
+              <p>Add a new event</p>
+            </button>
+            <button className={styles.command}>
+              <h2>Add Person</h2>
+              <p>Add a new person</p>
+            </button>
+            <button className={styles.command}>
+              <h2>Persons</h2>
+              <p>View all persons</p>
+            </button>
+          </div>
+        </div>
+        <footer className={styles.footer}>
+          <p>Made with <span>♥</span> by João Maranhão</p>
+        </footer>
+      </div>
+    </>
+  )
+}
